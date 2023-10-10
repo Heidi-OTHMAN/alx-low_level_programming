fa0f2f2376373d6602f28b34910607e462fffd72
Heidi-OTHMAN <heidiothman355@gmail.com>
2023-10-10 10:09:58 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,15 @@
 
 /**
  * _isalpha - a function that checks for alphabetic character
- *@c: checks input of function
+ *@c: takes input from other functions
  * Return: 1 if c is a letter, lowercase or uppercase
  *        otherwise returns 0
 */
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
+		return (1);
+	if (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
