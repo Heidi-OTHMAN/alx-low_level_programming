721263ade8bf78a0fcdd20da5469b1c831ea88a3
Heidi-OTHMAN <heidiothman355@gmail.com>
2023-10-12 12:54:04 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@
 
 void print_diagonal(int n)
 {
-	int postn, space;
+	int pos tn, space;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		for (pos tn = 1; pos tn <= n; pos tn++)
 		{
-			for (space = 1; space <= postn; space++)
+			for (space = 1; space <= pos tn; space++)
 				_putchar(' ');
 			_putchar(92); /*is equal to '/' char*/
 			_putchar('\n');
