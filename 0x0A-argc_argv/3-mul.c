56be66588acc7ee727aec392db04b7068ac21132
Heidi-OTHMAN <heidiothman355@gmail.com>
2023-10-27 11:58:22 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	output = malloc(sizeof(int *) *height);
+	output = malloc(sizeof(int *) * height);
 
 	if (output == NULL)
 		return (NULL);
