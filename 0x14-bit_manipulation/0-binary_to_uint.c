179012a3e1d2f2e6802a431e449dca35b1c09b93
Heidi-OTHMAN <heidiothman355@gmail.com>
2023-11-23 02:33:28 -0800

    a

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,19 +4,25 @@
  * binary_to_unit - converts a binary number string to unsigned int
  * @b: the binary number as a string
  *
- * Return: the unsigned int form of b
+ * Return: the unsigned int form of b, or 0 if error
 */
 unsigned int binary_to_unit(const char *b)
 {
+	int a;
 	unsigned int num = 0;
 
 	if (!b)
 		return (0);
-	while (*b)
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[a] != '0' && b[a] != '1')
 			return (0);
-		num = num * 2 + (*b++ - '0');
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
 	}
 	return (num);
 }
